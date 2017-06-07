94389c88cbe973e3eadaf0149b12b68cfb558add
Migara <migga1149@gmail.com>
2017-06-07 09:01:40 +0530

    html

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,7 @@ $(window).scroll(function(){
     var winscroll = $(this).scrollTop();
     var winheight = $(window).height();
 
-    $('.front-head').css({
-		'transform' : 'translate(0px, -'+ winscroll /4 +'%)'
+    $('.back-head').css({
+		'transform' : 'translate(0px, '+ winscroll /4 +'px)'
 	});
 });
-
-// Changing the defaults
-window.sr = ScrollReveal({ reset: true });
-
-// Customizing a reveal set
-sr.reveal('.para', {
-    duration: 200
-});
-sr.reveal('.foo', {
-  duration: 2000,
-  origin: 'top',
-  opacity: 1,
-});
-sr.reveal('.bar', { duration: 1000 });
